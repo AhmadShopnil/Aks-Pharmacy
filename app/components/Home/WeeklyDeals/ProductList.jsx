5c0a3fbc70625d41c59ddf83197a1be6a3f4f1ba
AhmadShopnil <ahmadshopnil75@gmail.com>
2026-01-27 10:34:27 +0600

    change product image

--- app/components/Home/WeeklyDeals/ProductList.jsx
+++ app/components/Home/WeeklyDeals/ProductList.jsx
@@ -4,47 +4,10 @@ import SmallProductCard from "../../Common/Cards/ProductCard/SmallProductCard";
 // ProductSection.jsx
 export default function ProductList({section_title}) {
  const products = [
-    
-    // {
-    //   id: 4,
-    //   title: "Aplb – Glutathione Niacinamide Beauty Tablet 30pcs",
-    //  img: "/images/medicine/4.jpg",
-    //   price: "780 ৳",
-    //   oldPrice: "1,010 ৳",
-    //   discount: "-23%",
-    //   rating: 4,
-    // },
-    //    {
-    //   id: 5,
-    //   title: "Cosrx – Advanced Snail 92 All In One Cream",
-    //   img: "/images/medicine/5.jpg",
-    //   price: "940 ৳",
-    //   oldPrice: "1,600 ৳",
-    //   discount: "-47%",
-    //   rating: 5,
-    // },
-    // {
-    //   id: 6,
-    //   title: "Essence – I Lo+ve Extreme Crazy Volume Mascara",
-    //  img: "/images/medicine/6.jpg",
-    //   price: "550 ৳",
-    //   oldPrice: "900 ৳",
-    //   discount: "-39%",
-    //   rating: 5,
-    // },
-    // {
-    //   id: 7,
-    //   title: "Ordinary – Niacinamide 10% + Zinc 1% – 30ml",
-    //   img: "/images/medicine/7.jpg",
-    //   price: "1,220 ৳",
-    //   oldPrice: "1,300 ৳",
-    //   discount: "-6%",
-    //   rating: 5,
-    // },
     {
       id: 1,
       title: "Cosrx – Advanced Snail 92 All In One Cream",
-      img: "/images/medicine/1.jpg",
+      img: "/images/medicine/23.webp",
       price: "940 ৳",
       oldPrice: "1,600 ৳",
       discount: "-47%",
@@ -53,7 +16,7 @@ export default function ProductList({section_title}) {
     {
       id: 2,
       title: "Essence – I Lo+ve Extreme Crazy Volume Mascara",
-     img: "/images/medicine/2.jpg",
+     img: "/images/medicine/24.jpeg",
       price: "550 ৳",
       oldPrice: "900 ৳",
       discount: "-39%",
@@ -62,7 +25,7 @@ export default function ProductList({section_title}) {
     {
       id: 3,
       title: "Ordinary – Niacinamide 10% + Zinc 1% – 30ml",
-      img: "/images/medicine/3.jpg",
+      img: "/images/medicine/13.jpeg",
       price: "1,220 ৳",
       oldPrice: "1,300 ৳",
       discount: "-6%",
@@ -71,7 +34,7 @@ export default function ProductList({section_title}) {
     {
       id: 8,
       title: "Aplb – Glutathione Niacinamide Beauty Tablet 30pcs",
-     img: "/images/medicine/8.jpg",
+     img: "/images/medicine/15.webp",
       price: "780 ৳",
       oldPrice: "1,010 ৳",
       discount: "-23%",
--- app/components/Shop/ProductGrid.jsx
+++ app/components/Shop/ProductGrid.jsx
@@ -3,21 +3,21 @@
 
 import ProductCardShop from "./ProductCardShop";
 
-  const products = [
-    
+const products = [
+
     {
       id: 4,
       title: "Aplb – Glutathione Niacinamide Beauty Tablet 30pcs",
-     img: "/images/medicine/4.jpg",
+      img: "/images/medicine/13.jpeg",
       price: "780 ৳",
       oldPrice: "1,010 ৳",
       discount: "-23%",
       rating: 4,
     },
-       {
+    {
       id: 5,
       title: "Cosrx – Advanced Snail 92 All In One Cream",
-      img: "/images/medicine/5.jpg",
+      img: "/images/medicine/14.webp",
       price: "940 ৳",
       oldPrice: "1,600 ৳",
       discount: "-47%",
@@ -26,7 +26,7 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 6,
       title: "Essence – I Lo+ve Extreme Crazy Volume Mascara",
-     img: "/images/medicine/6.jpg",
+      img: "/images/medicine/15.webp",
       price: "550 ৳",
       oldPrice: "900 ৳",
       discount: "-39%",
@@ -35,7 +35,7 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 7,
       title: "Ordinary – Niacinamide 10% + Zinc 1% – 30ml",
-      img: "/images/medicine/7.jpg",
+      img: "/images/medicine/16.jpeg",
       price: "1,220 ৳",
       oldPrice: "1,300 ৳",
       discount: "-6%",
@@ -44,7 +44,7 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 1,
       title: "Cosrx – Advanced Snail 92 All In One Cream",
-      img: "/images/medicine/1.jpg",
+      img: "/images/medicine/17.jpeg",
       price: "940 ৳",
       oldPrice: "1,600 ৳",
       discount: "-47%",
@@ -53,7 +53,7 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 2,
       title: "Essence – I Lo+ve Extreme Crazy Volume Mascara",
-     img: "/images/medicine/2.jpg",
+      img: "/images/medicine/18.webp",
       price: "550 ৳",
       oldPrice: "900 ৳",
       discount: "-39%",
@@ -62,7 +62,7 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 3,
       title: "Ordinary – Niacinamide 10% + Zinc 1% – 30ml",
-      img: "/images/medicine/3.jpg",
+      img: "/images/medicine/19.webp",
       price: "1,220 ৳",
       oldPrice: "1,300 ৳",
       discount: "-6%",
@@ -71,13 +71,34 @@ import ProductCardShop from "./ProductCardShop";
     {
       id: 8,
       title: "Aplb – Glutathione Niacinamide Beauty Tablet 30pcs",
-     img: "/images/medicine/8.jpg",
+      img: "/images/medicine/20.webp",
       price: "780 ৳",
       oldPrice: "1,010 ৳",
       discount: "-23%",
       rating: 4,
     },
+    {
+      id: 10,
+      title: "Essence – I Lo+ve Extreme Crazy Volume Mascara",
+      img: "/images/medicine/21.webp",
+      price: "550 ৳",
+      oldPrice: "900 ৳",
+      discount: "-39%",
+      rating: 5,
+    },
+    {
+      id: 9,
+      title: "Ordinary – Niacinamide 10% + Zinc 1% – 30ml",
+      img: "/images/medicine/22.webp",
+      price: "1,220 ৳",
+      oldPrice: "1,300 ৳",
+      discount: "-6%",
+      rating: 5,
+    },
   ];
+
+
+
 
 
 export default function ProductGrid() {
